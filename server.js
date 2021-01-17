d4f0676acc8485140fe437df069699771cdd9714
Ali Hinnawi <alihinnawi2014@gmail.com>
2021-01-17 16:11:26 +0100

    commit

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const buildpath = path.join(__dirname,'build')
+const port = process.env.PORT || 3000
 
 app.use(express.static(buildpath));
 app.get('*', function (req, res) {
     res.sendFile(path.join(buildpath, 'index.html'));
 });
 
-app.listen(3000);
+app.listen(port);
