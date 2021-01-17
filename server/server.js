c73bf562220b669c140399219581105edb5c554b
Ali Hinnawi <alihinnawi2014@gmail.com>
2021-01-17 14:58:16 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -1,17 +1,10 @@
-const path = require("path");
 const express = require("express");
+const path = require("path");
 const app = express();
-const publicPath = path.join(__dirname,'../','public');
-const port = process.env.PORT || 3000;
-
-app.use(express.static(publicPath))
-
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, ()=>{
-    console.log('server is up!')
-});
+app.listen(3000);
