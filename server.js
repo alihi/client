19d588ba25084488d596bb44ef6899a31e491b95
Ali Hinnawi <alihinnawi2014@gmail.com>
2021-01-17 15:50:46 +0100

    test

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const buildpath = path.join(__dirname,'build')
 
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(buildpath));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(buildpath, 'index.html'));
 });
 
 app.listen(3000);
